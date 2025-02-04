000a706115b4ca75aac60636096dea6d904f0e43
jharwick23 <jharwick12@gmail.com>
2025-02-03 22:32:56 -0500

    Update pong.cpp

--- pong.cpp
+++ pong.cpp
@@ -1,19 +1,32 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test");
-    
-    while (window.isOpen()) {
+int main()
+{
+    // Create a window with a default resolution of 800x600
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Pong");
+
+    // Main loop ends when window is closed
+    while (window.isOpen())
+    {
+        // Create event to hold even data
         sf::Event event;
-        while (window.pollEvent(event)) {
+
+        while (window.pollEvent(event))
+        {
+
+            // Check if the window close event was triggered
             if (event.type == sf::Event::Closed)
+            {
                 window.close();
+            }
         }
-        
+
+        // Clear the window before rendering a new frame
         window.clear();
+
         window.display();
     }
-    
+
     return 0;
 }
